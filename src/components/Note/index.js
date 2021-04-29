572ed122ae8f30868c2685bf08205cbae888f3f7
marisanesmith <marisa@thisworldtraveled.com>
2021-04-29 18:36:09 -0400

    created note taking app with react

---
+++ src/components/Note/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note() {
+    return (
+    <div className='note'>
+        <h1>Title</h1>
+        <p>This is going to be a very good note!</p>
+    </div>
+    )
+}
+
+export default Note
